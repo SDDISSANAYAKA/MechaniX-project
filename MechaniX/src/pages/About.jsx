27af887c683d516985c57c41dbc77297e972ebb8
Sachcha99 <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-02-28 14:15:16 +0530

    Update the About Page
    About.jsx and App.css

--- MechaniX/src/pages/About.jsx
+++ MechaniX/src/pages/About.jsx
@@ -3,9 +3,20 @@ import React from "react";
 function About() {
   return (
     <>
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>the MechnicX About us Page</h1>
+      <section className="hero-section-about">
+        <p>Welcome to </p><br></br>
+        <h1 className="h2-about">The MechnicX About Page</h1><br></br>
+        <p>We are a full-service auto repair shop committed to providing top-quality automotive services to our valued customers.
+
+At our automobile service, we understand the importance of reliable and safe transportation. That's why we have a team of experienced and certified technicians who are dedicated to providing the highest level of customer service. From routine maintenance to major repairs, we've got you covered.
+
+Our state-of-the-art facility is equipped with the latest diagnostic and repair equipment, allowing us to accurately diagnose and repair any issue with your vehicle. We use only the highest quality parts and materials, ensuring that your vehicle is restored to its optimal condition.
+
+We offer a wide range of services, including oil changes, brake repairs, engine diagnostics, transmission repairs, and much more. Our team will work closely with you to ensure that your vehicle is running smoothly and safely, and we'll keep you informed every step of the way.
+
+At our automobile service, we believe in building long-lasting relationships with our customers. We take pride in our honest and transparent approach, and we'll always provide you with the best possible service at a fair price.
+
+Thank you for choosing our automobile service for your automotive needs. We look forward to serving you and keeping your vehicle running smoothly for years to come.</p>
       </section>
     </>
   );
