2fe44694f8c012b9a5b181bac644a6608b0938b4
Sachcha99 <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-02-28 20:16:49 +0530

    Update Work.jsx

--- MechaniX/src/pages/Work.jsx
+++ MechaniX/src/pages/Work.jsx
@@ -6,16 +6,16 @@ function Work() {
       <p>Welcome to </p>
       <h1 className="h1-work">The MechnicX Work Page</h1>
      <ul className="ul-work">
-        <li>Routine maintenance: <span className="span-work">This includes services like oil changes, tire rotations, and brake inspections.</span></li>
-        <li>Repairs: <span className="span-work">Service centers can repair a variety of issues, such as engine problems, transmission issues, and electrical problems.</span></li>
-        <li>Diagnostic testing: <span className="span-work">Service centers use specialized equipment to diagnose problems with your vehicle's systems.</span></li>
-        <li>Tire services: <span className="span-work">This includes tire repairs, replacements, and alignments.</span></li>
-        <li>Suspension and steering services: <span className="span-work">This includes repairs and replacements of parts related to the suspension and steering systems.</span></li>
-        <li>Battery services: <span className="span-work">Service centers can test and replace your vehicle's battery.</span></li>
-        <li>Exhaust system services: <span className="span-work">This includes repairs and replacements of parts related to the exhaust system.</span></li>
-        <li>Air conditioning and heating services: <span className="span-work">Service centers can diagnose and repair issues with your vehicle's heating and cooling systems.</span></li>
-        <li>Fuel system services: <span className="span-work">This includes repairs and replacements of parts related to the fuel system.</span></li>
-        <li>Safety inspections: <span className="span-work">Service centers can perform safety inspections to ensure your vehicle is in compliance with local laws and regulations.</span></li>
+        <li>Routine maintenance: <span className="span-work">This includes services like oil changes, tire rotations, and brake inspections.</span></li><br/>
+        <li>Repairs: <span className="span-work">Service centers can repair a variety of issues, such as engine problems, transmission issues, and electrical problems.</span></li><br/>
+        <li>Diagnostic testing: <span className="span-work">Service centers use specialized equipment to diagnose problems with your vehicle's systems.</span></li><br/>
+        <li>Tire services: <span className="span-work">This includes tire repairs, replacements, and alignments.</span></li><br/>
+        <li>Suspension and steering services: <span className="span-work">This includes repairs and replacements of parts related to the suspension and steering systems.</span></li><br/>
+        <li>Battery services: <span className="span-work">Service centers can test and replace your vehicle's battery.</span></li><br/>
+        <li>Exhaust system services: <span className="span-work">This includes repairs and replacements of parts related to the exhaust system.</span></li><br/>
+        <li>Air conditioning and heating services: <span className="span-work">Service centers can diagnose and repair issues with your vehicle's heating and cooling systems.</span></li><br/>
+        <li>Fuel system services: <span className="span-work">This includes repairs and replacements of parts related to the fuel system.</span></li><br/>
+        <li>Safety inspections: <span className="span-work">Service centers can perform safety inspections to ensure your vehicle is in compliance with local laws and regulations.</span></li><br/>
       </ul>
     </div>
    
