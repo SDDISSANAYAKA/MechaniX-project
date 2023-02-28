9f41c1ce2d78fdbad6b2663b8f4b795d1b932d39
Sachcha99 <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-02-28 19:24:32 +0530

    Work Update
    work.jsx

--- MechaniX/src/pages/Work.jsx
+++ MechaniX/src/pages/Work.jsx
@@ -2,10 +2,23 @@ import React from "react";
 
 function Work() {
   return (
-    <div className="hero-section">
+    <div className="hero-section-work">
       <p>Welcome to </p>
-      <h1>the MechnicX Work Page</h1>
+      <h1 className="h1-work">The MechnicX Work Page</h1>
+     <ul className="ul-work">
+        <li>Routine maintenance: <span className="span-work">This includes services like oil changes, tire rotations, and brake inspections.</span></li>
+        <li>Repairs: <span className="span-work">Service centers can repair a variety of issues, such as engine problems, transmission issues, and electrical problems.</span></li>
+        <li>Diagnostic testing: <span className="span-work">Service centers use specialized equipment to diagnose problems with your vehicle's systems.</span></li>
+        <li>Tire services: <span className="span-work">This includes tire repairs, replacements, and alignments.</span></li>
+        <li>Suspension and steering services: <span className="span-work">This includes repairs and replacements of parts related to the suspension and steering systems.</span></li>
+        <li>Battery services: <span className="span-work">Service centers can test and replace your vehicle's battery.</span></li>
+        <li>Exhaust system services: <span className="span-work">This includes repairs and replacements of parts related to the exhaust system.</span></li>
+        <li>Air conditioning and heating services: <span className="span-work">Service centers can diagnose and repair issues with your vehicle's heating and cooling systems.</span></li>
+        <li>Fuel system services: <span className="span-work">This includes repairs and replacements of parts related to the fuel system.</span></li>
+        <li>Safety inspections: <span className="span-work">Service centers can perform safety inspections to ensure your vehicle is in compliance with local laws and regulations.</span></li>
+      </ul>
     </div>
+   
   );
 }
 
