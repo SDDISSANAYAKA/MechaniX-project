d104e8367d3a972025bc97f618ea0968e1cc6fb3
Sachcha99 <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-02-28 20:09:10 +0530

    contact Update

--- MechaniX/src/pages/Contact.jsx
+++ MechaniX/src/pages/Contact.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 function Contact() {
   return (
-    <>
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>the MechnicX Contact Page</h1>
-      </section>
-    </>
+    // <>
+    //   <section className="hero-section-contact">
+    //     <p>Welcome to </p>
+    //     <h1 className="h1-contact">The MechnicX Contact Page</h1>
+    //   </section>
+    // </>
+    <div className="div-contact">
+      <h1 className="h1-contact">The MechnicX Contact Page</h1>
+      <form>
+        <h1>Contact Us.</h1><br></br>
+        <input type='text' name="Name" id="" placeholder="Enter Your Name : " required></input><br></br>
+        <input type='email' name="Email" id="" placeholder="example@gmail.com" required></input><br></br>
+        <input type='phone' name="Phone" id="" placeholder="+94 77 2 588 588" required></input><br></br>
+        <textarea name="message" id="" cols='80' rows='10' placeholder="Type Here..." ></textarea><br></br>
+        <button type="submit">Send</button>
+      </form>
+    </div>
   );
 }
 
