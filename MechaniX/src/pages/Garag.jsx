0e8c0a3bfa376a58a9889b5533b09c87391f6bc9
Sachcha99 <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-02-28 19:57:44 +0530

    garage Update

--- MechaniX/src/pages/Garag.jsx
+++ MechaniX/src/pages/Garag.jsx
@@ -5,7 +5,7 @@ function Garage() {
   return (
     <div className="hero-section-garage">
       <p>Welcome to </p>
-      <h1>The MechnicX Garage.</h1>
+      <h1>The MechnicX Garage.</h1><br></br>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.569770834691!2d80.03835292695312!3d6.822062900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25270fc43835f%3A0xc04ff76f967f5c28!2sFaculty%20of%20Technology%2C%20University%20of%20Sri%20Jayewardenepura!5e0!3m2!1sen!2slk!4v1677544573427!5m2!1sen!2slk"
         width="600"
         height="450"
